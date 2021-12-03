a45d475fc4b120713c92ad238c7e6483cb88d8b9
dukeydd <d@dcd.im>
2021-12-03 23:03:30 +0000

    smaller dict

--- download_csv.py
+++ download_csv.py
@@ -2,9 +2,21 @@ import requests
 from io import BytesIO
 import zipfile 
 
-url = "https://www.bragitoff.com/wp-content/uploads/2016/03/Word-lists-in-csv.zip"
-resp = requests.get(url, stream=True).content
-zf = zipfile.ZipFile(BytesIO(resp))
-zf.extractall()
-# for line in zf.open(file).readlines():
-#     print(line.decode('utf-8'))
+
+def update_dict():
+    url = "http://www.gwicks.net/textlists/usa.zip"
+    resp = requests.get(url, stream=True).content
+    zf = zipfile.ZipFile(BytesIO(resp))
+    zf.extractall()
+
+
+def get_dict(filename='usa.txt'):
+    data = []
+    with open(filename, 'r') as dictionary:
+        for word in dictionary:
+            data.append(word.strip('\n').title())
+    
+    print(data[:5])
+    return data
+
+get_dict()
