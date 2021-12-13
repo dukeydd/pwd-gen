90e8526567d1d273bb35135bc6ced91971597169
dukeydd <d@dcd.im>
2021-12-13 23:08:49 +0000

    started get words endpoint

--- api/app.py
+++ api/app.py
@@ -11,7 +11,7 @@ def root():
     return {"Hello": "World"}
 
 
-@app.get("/get_words")
+@app.get("/get_all_words")
 def get_all_words():
     return {
         "words": get_words.get_words()[:20]
@@ -20,3 +20,13 @@ def get_all_words():
 @app.get("/update_dict")
 def update_dict():
     get_words.update_dict()
+
+@app.get("/get_word")
+def get_word(dice_numbers = []):
+    if not dice_numbers:
+        raise ValueError("No input dice numbers given")
+
+    if len(dice_numbers) < 7:
+        raise ValueError("Not enough dice numbers given")
+    
+    word_number = ((dice_numbers[0] - 1) * 16384) /4 
